33c9f7b017bdd1618c2aba005da8610dcfb3f625
Timothée CARON <timothee.caron@ecoles-epsi.net>
2025-03-06 12:38:33 +0100

    tp005 page 2

--- react-learn/vite.config.js
+++ react-learn/vite.config.js
@@ -1,10 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: true
-  }
-})
+function App() {
+  return "Hello World !"
+}
+export default App
