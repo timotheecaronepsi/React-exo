994955a74fa7b8b997c70073853ded575caf9636
Timothée CARON <timothee.caron@ecoles-epsi.net>
2025-03-10 16:49:00 +0100

    fin du chapitre 6

--- react-learn/src/App.jsx
+++ react-learn/src/App.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css"
 
 function App() {
@@ -6,14 +9,49 @@ function App() {
       <h1 className="titre-centre">Liste des tâches à faire</h1>
 
       <div className="button-container">
-      <button className="button-ajout">Ajouter + </button>
-      <button className="filter-select">Toutes ^ </button>
+        <button className="button-ajout">Ajouter + </button>
+        <button className="filter-select">Toutes ^ </button>
       </div>
 
       <div className="tache-containeur">
-        <span className="tache"></span>
+        <div className="tache">
+          <div className="task-container">
+            <input className="checkbox"></input>
+            <span className="task-text">Tache 1 </span>
+
+          </div>
+          <div className="button-actions">
+            <button className="modify-button"><FontAwesomeIcon icon={faEdit} /></button>
+            <button className="delete-button"><FontAwesomeIcon icon={faTrash} /></button>
+          </div>
+        </div>
+
+        <div className="tache">
+          <div className="task-container">
+            <input className="checkbox"></input>
+            <span className="task-text">Tache 2 </span>
+
+          </div>
+          <div className="button-actions">
+            <button className="modify-button"><FontAwesomeIcon icon={faEdit} /></button>
+            <button className="delete-button"><FontAwesomeIcon icon={faTrash} /></button>
+          </div>
+        </div>
+
+        <div className="tache">
+          <div className="task-container">
+            <input className="checkbox"></input>
+            <span className="task-text">Tache 3 </span>
+
+          </div>
+          <div className="button-actions">
+            <button className="modify-button"><FontAwesomeIcon icon={faEdit} /></button>
+            <button className="delete-button"><FontAwesomeIcon icon={faTrash} /></button>
+          </div>
+        </div>
+
       </div>
     </>
   )
 }
-export default App 
+export default App
